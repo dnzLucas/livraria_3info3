c0bd70b855db2b7239fd32310f3731415f9827f6
dnzlucas <lucasdefenix@gmail.com>
2025-05-20 09:30:31 -0300

    feat: implementação do módulo uploader com suporte a upload de imagens e documentos

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -8,6 +10,7 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 
 from core.views import AutorViewSet, EditoraViewSet, UserViewSet, CategoriaViewSet, LivroViewSet
+from uploader.router import router as uploader_router
 
 router = DefaultRouter()
 
@@ -32,6 +35,15 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name='schema'),
         name='redoc',
     ),
+    # uploader
+    path(
+        "api/media/",
+        include(uploader_router.urls)),  
     # API
-    path('api/', include(router.urls)),
+    path(
+        'api/',
+        include(router.urls)),
+
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
