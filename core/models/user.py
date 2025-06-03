5691b49e4c02fdeeac6cbdea2fba596f215a6846
dnzlucas <lucasdefenix@gmail.com>
2025-06-03 09:26:38 -0300

    feat: inclusão da foto de perfil no usuário

--- core/models/user.py
+++ core/models/user.py
@@ -4,12 +4,14 @@ Database models.
 
 from django.contrib.auth.models import (
     AbstractBaseUser,
+    AbstractUser,
     BaseUserManager,
     PermissionsMixin,
 )
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from uploader.models import Image
 
 class UserManager(BaseUserManager):
     """Manager for users."""
@@ -43,6 +45,14 @@ class User(AbstractBaseUser, PermissionsMixin):
     passage_id = models.CharField(max_length=255, unique=True, verbose_name=_('passage_id'), help_text=_('Passage ID'))
     email = models.EmailField(max_length=255, unique=True, verbose_name=_('email'), help_text=_('Email'))
     name = models.CharField(max_length=255, blank=True, null=True, verbose_name=_('name'), help_text=_('Username'))
+    foto = models.ForeignKey(
+            Image,
+            related_name="+",
+            on_delete=models.SET_NULL,
+            null=True,
+            blank=True,
+            default=None,
+    )
     is_active = models.BooleanField(
         default=True, verbose_name=_('Usuário está ativo'), help_text=_('Indica que este usuário está ativo.')
     )
@@ -51,6 +61,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name=_('Usuário é da equipe'),
         help_text=_('Indica que este usuário pode acessar o Admin.'),
     )
+
 
     objects = UserManager()
 
@@ -62,3 +73,4 @@ class User(AbstractBaseUser, PermissionsMixin):
 
         verbose_name = 'Usuário'
         verbose_name_plural = 'Usuários'
+
